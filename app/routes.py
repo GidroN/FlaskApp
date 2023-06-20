cace9304c399b6a934aea33b232a35cf90843abe
Anton Illenberger <gidrontv1@gmail.com>
2023-06-20 22:41:53 +0400

    project getting bigger =)

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 from flask import render_template
 from app import app
 
@@ -6,4 +5,18 @@ from app import app
 @app.route('/index')
 def index():
     user = {'username': 'Miguel'}
-    return render_template('index.html', title='Home', user=user)
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }, 
+        {
+            'author': {'username': 'Ипполит'},
+            'body': 'Какая гадость эта ваша заливная рыба!!'
+        }
+    ]
+    return render_template('index.html', user=user, title='title', posts=posts)
